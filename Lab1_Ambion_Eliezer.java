db6457a93f24de68ab54e742d186d3953fbeae6b
Onoiski08 <57052385+Onoiski08@users.noreply.github.com>
2020-11-20 14:08:58 +0800

    sample programs in java

---
+++ Lab1_Ambion_Eliezer.java
@@ -0,0 +1,24 @@
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("I am writing a program that will display \"text\" with escape characters.\t\tAt the beginning of this sentence it has tab escape character.\n\nThere are two new lines before this sentence and you also have to include 'single quote' and \\back slash\\ characters in this sentence.\"");
+			
+		float Gross = 0.0f;
+		byte Age = 1;
+		boolean Final = false;
+		byte Location = 17;
+		byte OldFile = 55;
+		char Default = 'a';
+		String name = "noname";
+		
+		System.out.println (Gross);
+		System.out.println (Age);
+		System.out.println (Final);
+		System.out.println (Location);
+		System.out.println (OldFile);
+		System.out.println (Default);
+		System.out.println (name);
+	}
+
+}
